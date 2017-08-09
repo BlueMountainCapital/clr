bf180f4f98e80502cb0209067d685f8c868d2dd3
Rickard Holmberg <rickard@avkrok.net>
2017-08-09 17:41:28 -0400

    Add test throwing exception with inner exception in iterator
    (cherry picked from commit 4956b3e)
    
    (cherry picked from commit d119c6c)

--- src/tests/test_exceptions.py
+++ src/tests/test_exceptions.py
@@ -382,7 +382,9 @@ def test_iteration_exception():
     from Python.Test import ExceptionTest
     from System import OverflowException
 
-    val = ExceptionTest.ThrowExceptionInIterator().__iter__()
+    exception = OverflowException("error")
+
+    val = ExceptionTest.ThrowExceptionInIterator(exception).__iter__()
     assert next(val) == 1
     assert next(val) == 2
     with pytest.raises(OverflowException) as cm:
@@ -390,4 +392,20 @@ def test_iteration_exception():
 
     exc = cm.value
 
-    assert isinstance(exc, OverflowException)
+    assert exc == exception
+
+def test_iteration_innerexception():
+    from Python.Test import ExceptionTest
+    from System import OverflowException
+
+    exception = System.Exception("message", OverflowException("error"))
+
+    val = ExceptionTest.ThrowExceptionInIterator(exception).__iter__()
+    assert next(val) == 1
+    assert next(val) == 2
+    with pytest.raises(OverflowException) as cm:
+        next(val)
+
+    exc = cm.value
+
+    assert exc == exception.InnerException
